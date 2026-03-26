aa0538095ffb738bc6fcae78aa67aebaaa07267b
buiqqq11122 <buiqq11122@gmail.com>
2026-03-26 15:10:50 +0700

    add

---
+++ src/main/java/com/example/QLDuAn/QlDuAnApplication.java
@@ -0,0 +1,13 @@
+package com.example.QLDuAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlDuAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlDuAnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QLDuAn/QlDuAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QLDuAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlDuAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
